a1aeaf6bbc3ef2267ba2844b4f1f35f585b11c68
webdesc <webdesc@gmail.com>
2019-11-13 08:01:01 +0300

    add welcome user

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
+import welcome from '..';
+
 console.log('Welcome to the Brain Games!');
+
+const username = welcome();
+
+if (username) {
+  console.log('Hi ' + username + '!');
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import readLineSync from 'readline-sync';
+
+export default () => readLineSync.question('May I have your name? ');
